6a8785dc73092a19e090a5fdd95804cbfb15abac
Yesuf Fenta <yesuf023@gmail.com>
2023-10-14 01:09:00 +0300

    Update decode_morse_message.rb

--- decode_morse_message.rb
+++ decode_morse_message.rb
@@ -1,9 +1,6 @@
 # Here is a method in Ruby that decodes a Morse code character and returns the corresponding character in uppercase.
 # I use this method to help you decode a Morse code message.
 
-# Example:
-# decode_char('...') => 'S'
-# decode_char('-.-.') => 'C'
 def decode_char(morse_code)
   morse_code_dict = {
     '.-' => 'A',
