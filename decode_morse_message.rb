7f4702309524713f6bda3b70f325924c5be94448
yusufyusie <yesuf023@gmail.com>
2023-10-13 23:46:24 +0300

    Add decoder of chars to capletters

---
+++ decode_morse_message.rb
@@ -0,0 +1,38 @@
+# Here is a method in Ruby that decodes a Morse code character and returns the corresponding character in uppercase.
+# I use this method to help you decode a Morse code message.
+
+# Example:
+# decode_char('...') => 'S'
+# decode_char('-.-.') => 'C'
+def decode_char(morse_code)
+  morse_code_dict = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+
+  morse_code_dict[morse_code] || '[UNKNOWN]'
+end
